e988b1ffc95513b27ff8c883c15d2045cd890ca6
Manya Kalia <manyakalia10@gmail.com>
2026-08-27 22:25:28 +0530

    Time: 4 ms (36.55%) | Memory: 43.1 MB (73.09%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='{'){
+                st.push('}');
+            }
+            else if(ch=='['){
+                st.push(']');
+            }
+            else if(ch=='('){
+                st.push(')');
+            }
+            else if(ch=='}'||ch==']'||ch==')'){
+                if(st.isEmpty()||st.pop()!=ch){
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+        
+    }
+}
